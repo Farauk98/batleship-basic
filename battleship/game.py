5020b7b26132408da2054b5b7ad3d2c5db61ee46
rafal.kornat <kornat.rafal@gmail.com>
2024-09-25 15:11:19 +0200

    update code game.py

--- battleship/game.py
+++ battleship/game.py
@@ -1,7 +1,9 @@
 import random
-
 from battleship.ship import Ship
-from battleship.strategy import Strategy, RandomStrategy, HuntAndTargetStrategy, ProbabilityBetterStrategy, ProbabilityBetterChessboardStrategy, ProbabilityBetterSmallerSquareStrategy
+from battleship.strategy import (Strategy, RandomStrategy, HuntAndTargetStrategy,
+                                  ProbabilityBetterStrategy, ProbabilityBetterChessboardStrategy,
+                                  ProbabilityBetterSmallerSquareStrategy)
+
 
 class Game:
     def __init__(self, board_size=10, strategy=None):
@@ -9,52 +11,45 @@ class Game:
         self.board = [[" " for _ in range(board_size)] for _ in range(board_size)]
         self.ships = []
         self.shot_count = 0
-        self.strategy = strategy(self) if strategy else RandomStrategy(self)
+        self.strategy = (strategy(self) if strategy else RandomStrategy(self))
         self.shots = []
 
-    def display_board(self,reveal=False):
-        print("  " + " ".join([str(i) for i in range(self.board_size)]))
+    def display_board(self, reveal=False):
+        header = "  " + " ".join(map(str, range(self.board_size)))
+        print(header)
         for i in range(self.board_size):
-            if reveal:
-                print(str(i) + " " + " ".join(self.board[i]))
-            else:
-                print(str(i) + " " + " ".join(["S" if x == "S" else x for x in self.board[i]]))
+            row = " ".join(self.board[i] if reveal else ["S" if x == "S" else x for x in self.board[i]])
+            print(f"{i} {row}")
 
     def add_ship(self, size):
         ship = Ship(size)
-        placed = False
-        while not placed:
-            direction = random.choice(["H", "V"])
-            start_row = random.randint(0, self.board_size - 1)
-            start_col = random.randint(0, self.board_size - 1)
-            placed = ship.place_ship(self.board, start_row, start_col, direction)
+        while not ship.place_ship(self.board, *self.random_position(), random.choice(["H", "V"])):
+            pass
         self.ships.append(ship)
 
+    def random_position(self):
+        return random.randint(0, self.board_size - 1), random.randint(0, self.board_size - 1)
+
     def take_shot(self, row, col):
-        if row < 0 or row >= self.board_size or col < 0 or col >= self.board_size:
-            #print("Poza planszą!")
+        if not self.is_within_bounds(row, col) or self.is_already_shot(row, col):
             return False, False
 
-        if self.board[row][col] == "X" or self.board[row][col] == "O":
-            #print("Już tutaj strzelałeś!")
-            return False, False
+        hit, is_sunk = self.check_hit(row, col)
+        self.board[row][col] = "X" if hit else "O"
+        return hit, is_sunk
 
-        hit = False
-        is_sunk = False
+    def is_within_bounds(self, row, col):
+        return 0 <= row < self.board_size and 0 <= col < self.board_size
+
+    def is_already_shot(self, row, col):
+        return self.board[row][col] in ["X", "O"]
+
+    def check_hit(self, row, col):
         for ship in self.ships:
             if ship.is_hit(row, col):
-                #print("Trafiony!")
-                self.board[row][col] = "X"
-                hit = True
-                self.last_hit = (row, col)  # Ustaw ostatnie trafienie
-                if ship.is_sunk():
-                    is_sunk = True
-                break
-
-        if not hit:
-            self.board[row][col] = "O"
-
-        return hit, is_sunk
+                self.last_hit = (row, col)
+                return True, ship.is_sunk()
+        return False, False
 
     def is_game_over(self):
         return all(ship.is_sunk() for ship in self.ships)
@@ -65,8 +60,3 @@ class Game:
 
         while not self.is_game_over():
             self.strategy.take_turn()
-            # self.display_board(reveal=True)
-            # self.display_board()
-        
-        # self.display_board(reveal=True)
-        # print("ENDGASME")
